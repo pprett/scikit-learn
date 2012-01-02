4a427a573e707d65b12dbd2aba69a3470f8269a5
Noel Dawe <Noel.Dawe@cern.ch>
2012-01-02 18:13:58 -0400

    example

--- examples/ensemble/plot_boost_error.py
+++ examples/ensemble/plot_boost_error.py
@@ -16,32 +16,54 @@ from sklearn.datasets import make_blobs
 from sklearn.ensemble import BoostedClassifier
 from sklearn.tree import DecisionTreeClassifier
 
-# Build a classification task
-X, y = make_blobs(n_samples=100000,
-                  n_features=10,
-                  cluster_std=10,
-                  centers=3)
+import scipy.stats
 
-X_test, X_train = X[:50000], X[50000:]
-y_test, y_train = y[:50000], y[50000:]
+# Build multivariate normal distribution
+n_features = 10
+n_samples = 13000
+n_split = 3000
+
+cov = np.diag(np.ones(n_features))
+mean = np.zeros(n_features)
+X = np.random.multivariate_normal(mean, cov, n_samples)
+
+# sort by distance from origin
+X = np.array(sorted(list(X), key=lambda x: sum([x_i**2 for x_i in x])))
+
+
+# label by quantile
+y = []
+for i, x in enumerate(X):
+    if i < n_samples / 3.:
+        y.append(1)
+    elif i < 2 * n_samples / 3.:
+        y.append(2)
+    else:
+        y.append(3)
+
+y = np.array(y)
+
+# random permutation
+perm = np.random.permutation(n_samples)
+y = y[perm]
+X = X[perm]
+
+X_train, X_test = X[:n_split], X[n_split:]
+y_train, y_test = y[:n_split], y[n_split:]
 
 test_errors = []
 train_errors = []
 
-
-bdt = BoostedClassifier(DecisionTreeClassifier(min_split=100),
-                        n_estimators=10)
+bdt = BoostedClassifier(DecisionTreeClassifier(min_split=10),
+                        n_estimators=100)
 
 for boost in bdt.fit_generator(X_train, y_train):
-
-    print boost.n_estimators, len(boost.estimators_)
 
     y_test_predict = boost.predict(X_test)
     y_train_predict = boost.predict(X_train)
 
     test_errors.append(sum(y_test_predict != y_test) / float(len(y_test)))
     train_errors.append(sum(y_train_predict != y_train) / float(len(y_train)))
-
 
 # Plot the feature importances of the trees and of the forest
 import pylab as pl
