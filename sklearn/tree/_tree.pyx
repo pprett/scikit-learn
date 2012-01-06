8bd6d8405befc49c9cd201de5531b4fe0f4e4cf1
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2012-01-06 18:59:26 +0100

    fix up tree cython module (take 2)

--- sklearn/tree/_tree.pyx
+++ sklearn/tree/_tree.pyx
@@ -439,7 +439,10 @@ cdef int smallest_sample_larger_than(int sample_idx, DTYPE_t *X_i,
         -1 if no such element exists.
     """
     cdef int idx = 0, j
-    cdef DTYPE_t threshold = X_i[X_argsorted_i[sample_idx]]
+    cdef DTYPE_t threshold = -DBL_MAX
+
+    if sample_idx > -1:
+        threshold = X_i[X_argsorted_i[sample_idx]]
 
     for idx from sample_idx < idx < n_total_samples:
         j = X_argsorted_i[idx]
@@ -512,7 +515,7 @@ def _find_best_split(np.ndarray[DTYPE_t, ndim=2, mode="fortran"] X,
     cdef int n_features = X.shape[1]
     cdef int i, a, b, best_i = -1
     cdef DTYPE_t t, initial_error, error
-    cdef DTYPE_t best_error = np.inf, best_t = np.nan
+    cdef DTYPE_t best_error = np.inf, best_t = np.inf
     cdef DTYPE_t *y_ptr = <DTYPE_t *>y.data
     cdef DTYPE_t *X_i = NULL
     cdef int *X_argsorted_i = NULL
@@ -535,7 +538,7 @@ def _find_best_split(np.ndarray[DTYPE_t, ndim=2, mode="fortran"] X,
     if initial_error == 0:  # break early if the node is pure
         return best_i, best_t, initial_error, initial_error
 
-    best_error = np.inf ##initial_error
+    best_error = initial_error
 
     # Features to consider
     if max_features == n_features:
