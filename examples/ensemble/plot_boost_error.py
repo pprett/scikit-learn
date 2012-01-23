5700b72c5ab485fd5a382ad44867bab3c75461dc
Noel Dawe <Noel.Dawe@cern.ch>
2012-01-22 22:47:23 -0800

    add min_leaf (minimum size of leaf node) to decision tree

--- examples/ensemble/plot_boost_error.py
+++ examples/ensemble/plot_boost_error.py
@@ -59,7 +59,7 @@ y_train, y_test = y[:n_split], y[n_split:]
 test_errors = []
 train_errors = []
 
-bdt = BoostedClassifier(DecisionTreeClassifier(min_split=600),
+bdt = BoostedClassifier(DecisionTreeClassifier(min_leaf=100),
                         n_estimators=600)
 
 bdt.fit(X_train, y_train, verbose=True)
