1bf2eb209d4caa33cd6abd8fe049eea9743e7971
Noel Dawe <Noel.Dawe@cern.ch>
2012-01-23 11:15:22 -0800

    unneeded import

--- examples/ensemble/plot_boost_error.py
+++ examples/ensemble/plot_boost_error.py
@@ -11,7 +11,6 @@ print __doc__
 
 import numpy as np
 
-from sklearn.datasets import make_blobs
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets.samples_generator import make_gaussian_quantiles
