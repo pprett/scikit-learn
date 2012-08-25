dd224a434371bbdf27531a74155f82f9af5c9318
Noel Dawe <noel.dawe@cern.ch>
2012-08-26 00:55:08 +0200

    refactor weight boosting

--- sklearn/ensemble/__init__.py
+++ sklearn/ensemble/__init__.py
@@ -8,6 +8,7 @@ from .forest import RandomForestClassifier
 from .forest import RandomForestRegressor
 from .forest import ExtraTreesClassifier
 from .forest import ExtraTreesRegressor
-from .boosting import  AdaBoostClassifier
+from .weight_boosting import  AdaBoostClassifier
+from .weight_boosting import  AdaBoostRegressor
 from .gradient_boosting import GradientBoostingClassifier
 from .gradient_boosting import GradientBoostingRegressor
