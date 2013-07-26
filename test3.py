ae47536277ed4969b11b2762c43dae64df1a0e66
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2013-07-26 11:22:38 +0200

    test for 32bit issue

---
+++ test3.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from sklearn.datasets import load_boston
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.cross_validation import train_test_split
+
+boston = load_boston()
+
+X_train, X_test, y_train, y_test = train_test_split(boston.data, boston.target,
+                                                    random_state=1)
+
+est = DecisionTreeRegressor(random_state=1)
+
+est.fit(X_train, y_train)
+print np.diff(y_train).sum()
+print est.score(X_test, y_test)
